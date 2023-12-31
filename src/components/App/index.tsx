648814b7f57d3c8ae923bd85c81ca709d597f221
carpediem511 <vampire05@yandex.ru>
2023-12-31 16:03:09 +0300

    готов App черновик

--- src/components/App/index.tsx
+++ src/components/App/index.tsx
@@ -1,10 +1,37 @@
+import { useState } from 'react';
+import { Data, gameTypes } from '../Data';
 import './../../index.css';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import StartPage from '../StartPage';
+import GamePage from '../GamePage';
+import ResultsPage from '../ResultsPage';
 
-function App() {
+type AppProps = {
+	getCards: (type: number) => Data[],
+	results: { name: string, stepsCount: number }[]
+}
+
+function App({ getCards, results }: AppProps) {
+
+	const [gameTheme, setGameTheme] = useState<string>('')
+	const [cards, setCards] = useState<Data[]>([])
+
+	const handleStart = (type: string): void => {
+		const themeIndex = gameTypes.findIndex(t => t.type === type)
+		const selectedTheme = getCards(themeIndex)
+		setGameTheme(type)
+		setCards(selectedTheme)
+	}
 
 	return (
 		<>
-
+			<BrowserRouter>
+				<Routes>
+					<Route path='/' element={<StartPage setGameTheme={setGameTheme} handleStart={handleStart} />} />
+					<Route path='/game/:gameTheme' element={<GamePage cards={cards} />} />
+					<Route path='/result' element={<ResultsPage results={results} />} />
+				</Routes>
+			</BrowserRouter>
 		</>
 	)
 }
