c2a49bb1749f9c5f80d6929fba518c33ab9db981
carpediem511 <vampire05@yandex.ru>
2023-12-31 16:12:07 +0300

    дополнила App

--- src/components/App/index.tsx
+++ src/components/App/index.tsx
@@ -1,7 +1,7 @@
 import { useState } from 'react';
 import { Data, gameTypes } from '../Data';
 import './../../index.css';
-import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import { BrowserRouter, Route, Routes, useNavigate } from 'react-router-dom';
 import StartPage from '../StartPage';
 import GamePage from '../GamePage';
 import ResultsPage from '../ResultsPage';
@@ -15,6 +15,8 @@ function App({ getCards, results }: AppProps) {
 
 	const [gameTheme, setGameTheme] = useState<string>('')
 	const [cards, setCards] = useState<Data[]>([])
+	const [result, setResult] = useState<number>(0)
+	const navigate = useNavigate()
 
 	const handleStart = (type: string): void => {
 		const themeIndex = gameTypes.findIndex(t => t.type === type)
@@ -23,13 +25,22 @@ function App({ getCards, results }: AppProps) {
 		setCards(selectedTheme)
 	}
 
+	const showResults = (stepsCount: number) => {
+		setResult(stepsCount)
+		navigate('/results')
+	}
+
+	const handleRestart = () => {
+		navigate('/')
+	}
+
 	return (
 		<>
 			<BrowserRouter>
 				<Routes>
 					<Route path='/' element={<StartPage setGameTheme={setGameTheme} handleStart={handleStart} />} />
-					<Route path='/game/:gameTheme' element={<GamePage cards={cards} />} />
-					<Route path='/result' element={<ResultsPage results={results} />} />
+					<Route path='/game/:gameTheme' element={<GamePage cards={cards} gameTheme={gameTheme} showResults={showResults} />} />
+					<Route path='/result' element={<ResultsPage stepsCount={result} onResetGame={handleRestart} results={results} />} />
 				</Routes>
 			</BrowserRouter>
 		</>
