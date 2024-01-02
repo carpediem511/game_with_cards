55ccef81ad6d197e80c4b6c6b1027a159518d53c
carpediem511 <vampire05@yandex.ru>
2024-01-02 23:06:00 +0300

    еще раз сначала

--- src/components/App/index.tsx
+++ src/components/App/index.tsx
@@ -1,23 +1,11 @@
-import { useState } from 'react';
-import { TypeForGetCard, gameThemes, getCards, TypeForGameTheme, cats, parrots, forKids } from '../Data';
-import StartPage from '../StartPage';
 import './../../index.css';
-import { Route, Routes, useNavigate } from 'react-router-dom';
-import GamePage from '../GamePage';
 
-type AppProps = { getCards: (type: number) => TypeForGetCard[] }
 
-function App({ getCards }: AppProps) {
-
-	const [gameTheme, setGameTheme] = useState<TypeForGameTheme | null>(null)
-	const navigate = useNavigate()
+function App() {
 
 	return (
 		<>
-			<Routes>
-				<Route path='/' element={<StartPage setGameTheme={setGameTheme} />} />
-				<Route path='/game/:type' element={<GamePage gameTheme={gameTheme} getCards={{ "cats": cats, "parrots": parrots, "forKids": forKids }} />} />
-			</Routes>
+
 		</>
 	)
 }
--- src/components/Cards/index.tsx
+++ src/components/Cards/index.tsx
@@ -1,31 +1,8 @@
-type CardsProps = { handleCardClick: (id: string) => void }
-
-const Cards = ({ handleCardClick }: CardsProps) => {
+const Cards = () => {
 
 	return (
 		<>
-			<>
-				<li
-					className={`card `}
-					style={{
-						//transition: `transform ${animationDuration}`,
-						//backgroundImage: isOpenCard ? `url(${backImageUrl})` : "none",
-					}}
-				//onClick={handleClick}
-				>
 
-					<img
-						//src={url}
-						width="204"
-						height="144"
-					//alt={description}
-					//style={{ transform, backfaceVisibility: "hidden" }}
-					/>
-
-					<div className="card-back"></div>
-
-				</li>
-			</>
 		</>
 	)
 }
