c3ea0e21af5728de1d09db3d2d57b50bafbb1837
carpediem511 <vampire05@yandex.ru>
2024-01-10 22:10:18 +0300

    адаптив

--- src/components/StartPage/index.tsx
+++ src/components/StartPage/index.tsx
@@ -3,49 +3,52 @@ import { gameThemes, TypeForGameTheme } from "../Data";
 import { useState } from "react";
 
 const StartPage = () => {
-  const [, setSelectedTheme] = useState<TypeForGameTheme | null>(null);
-  const navigate = useNavigate();
+	const [, setSelectedTheme] = useState<TypeForGameTheme | null>(null);
+	const navigate = useNavigate();
 
-  const handleStartGame = (theme: TypeForGameTheme) => {
-    setSelectedTheme(theme);
-    navigate(`/game/${theme.type}`);
-  };
+	const handleStartGame = (theme: TypeForGameTheme) => {
+		setSelectedTheme(theme);
+		navigate(`/game/${theme.type}`);
+	};
 
-  return (
-    <section className="rules container">
-      <h2>Добро пожаловать!</h2>
-      <p>Мозаика памяти — игра для тренировки визуальной памяти</p>
-      <div className="rules-panel">
-        <h3>Правила игры</h3>
-        <ul className="rules-list">
-          <li>
-            В наборе есть множество карточек – по две штуки с одним и тем же
-            рисунком.
-          </li>
-          <li>
-            Нужно разложить карточки «рубашкой» вверх на столе, а затем
-            переворачивать по две.
-          </li>
-          <li>
-            Если они совпадают – игрок забирает их и получает ещё один ход.
-          </li>
-        </ul>
-      </div>
+	return (
+		<section className="rules container">
+			<h2>Добро пожаловать!</h2>
+			<p>Мозаика памяти — игра для тренировки визуальной памяти</p>
+			<div className="rules-panel">
+				<h3>Правила игры</h3>
+				<ul className="rules-list">
+					<li>
+						В наборе есть множество карточек – по две штуки с одним и тем же
+						рисунком.
+					</li>
+					<li>
+						Нужно разложить карточки «рубашкой» вверх на столе, а затем
+						переворачивать по две.
+					</li>
+					<li>
+						Если они совпадают – игрок забирает их и получает ещё один ход.
+					</li>
+				</ul>
+			</div>
 
-      <div>
-        <h3>Выберите тему игры:</h3>
-        {gameThemes.map(({ type, text }) => (
-          <button
-            key={type}
-            className={`ico-button ico-button-${type}`}
-            onClick={() => handleStartGame({ type, text: text })}
-          >
-            {text}
-          </button>
-        ))}
-      </div>
-    </section>
-  );
+			<div>
+				<h3>Выберите тему игры:</h3>
+				<div className="flex w-1/2 mx-auto">
+					{gameThemes.map(({ type, text }) => (
+
+						<button
+							key={type}
+							className={`ico-button ico-button-${type}`}
+							onClick={() => handleStartGame({ type, text: text })}
+						>
+							{text}
+						</button>
+					))}
+				</div>
+			</div>
+		</section>
+	);
 };
 
 export default StartPage;
